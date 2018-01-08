f62902d1ac898ce1088ffff8e318adb2584b41de
gaoyang <gaoyang@joycastle.mobi>
2018-01-08 18:59:10 +0800

    Fix Android.mk

--- Android.mk
+++ Android.mk
@@ -7,6 +7,7 @@ LOCAL_MODULE := pomelo_static
 LOCAL_MODULE_FILENAME := libpomelo
 
 LOCAL_SRC_FILES := \
+src/pc_JSON.c \
 src/pc_lib.c \
 src/pc_pomelo.c \
 src/pc_trans.c \
@@ -14,15 +15,14 @@ src/pc_trans_repo.c \
 src/tr/dummy/tr_dummy.c \
 src/tr/uv/pb_decode.c \
 src/tr/uv/pb_encode.c \
-src/tr/uv/pb_util.c \
+src/tr/uv/pb_i.c \
 src/tr/uv/pr_msg.c \
 src/tr/uv/pr_msg_json.c \
 src/tr/uv/pr_msg_pb.c \
 src/tr/uv/pr_pkg.c \
 src/tr/uv/tr_uv_tcp.c \
 src/tr/uv/tr_uv_tcp_aux.c \
-src/tr/uv/tr_uv_tcp_i.c
-
+src/tr/uv/tr_uv_tcp_i.c \
 
 LOCAL_CFLAGS := -DPC_NO_UV_TLS_TRANS
 
@@ -37,7 +37,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
 					$(LOCAL_PATH)/src/tr/dummy \
 					$(LOCAL_PATH)/src/tr/uv
 
-LOCAL_WHOLE_STATIC_LIBRARIES := uv_static jansson_static
+# LOCAL_WHOLE_STATIC_LIBRARIES := uv_static jansson_static
+LOCAL_WHOLE_STATIC_LIBRARIES := uv_static
 
 
 
@@ -45,5 +46,5 @@ include $(BUILD_STATIC_LIBRARY)
 
 LOCAL_CFLAGS    := -D__ANDROID__ 
 
-$(call import-module,libpomelo2/deps/uv) \
-$(call import-module,libpomelo2/deps/jansson)
+$(call import-module,libpomelo2/deps/uv)
+# $(call import-module,libpomelo2/deps/jansson)
